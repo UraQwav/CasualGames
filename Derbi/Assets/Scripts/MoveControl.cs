a1b12a0dd30f8ca291b6038c3bd13529395d4a32
UraQwav <ura.qwav@gmail.com>
2020-02-23 22:51:19 +0300

    First design&move control

--- Derbi/Assets/Scripts/MoveControl.cs
+++ Derbi/Assets/Scripts/MoveControl.cs
@@ -1,31 +1,15 @@
-﻿using UnityEngine;
-using UnityEngine.EventSystems;
-
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
 public class MoveControl : MonoBehaviour
 {
-    public bool _isMoving;
-    public float speed = 8f;
-    private Vector3 touchPosition;
-    private Vector3 direction;
-    private Rigidbody rigidbody;
-    void Start()
-    {
-        rigidbody = GetComponent<Rigidbody>();
-    }
-    void Update()
-    {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
-            touchPosition = Camera.main.ScreenToWorldPoint(touch.position);
-            touchPosition.z = 0;
-            direction = (touchPosition - transform.position);
-            rigidbody.velocity = new Vector2(direction.x, direction.y) * speed;
-            if (touch.phase == TouchPhase.Ended)
-                rigidbody.velocity = Vector2.zero;
-            _isMoving = true;
-        }
-        else _isMoving = false;
-    }
+    public float speed;
+    public FloatingJoystick joystick;
+    public Rigidbody rb;
 
+    public void FixedUpdate()
+    {
+        Vector3 direction = Vector3.forward * joystick.Vertical + Vector3.right * joystick.Horizontal;
+        rb.AddForce(direction * speed * Time.fixedDeltaTime, ForceMode.VelocityChange);
+    }
 }
